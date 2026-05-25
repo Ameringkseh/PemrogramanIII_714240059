9e8dd188c78d40164b290924b4a824563edd4bb6
Rafli <raflimramadhan.204@gmail.com>
2026-05-25 15:47:47 +0700

    add ss

---
+++ Pertemuan10/Tugas/be_latihan/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"be_latihan/config"
+	"be_latihan/model"
+	"be_latihan/router"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(logger.New())
+
+	//inisialisasi database
+	config.InitDB()
+
+	config.GetDB().AutoMigrate(&model.Mahasiswa{})
+	router.SetupRoutes(app)
+
+	app.Listen(":3000")
+}
